8b34b0ddfa6dc062f25d9b35a96c27a567c8fa14
Jonatas Freitas <jonatasbmf@gmail.com>
2024-10-29 11:53:25 -0300

    Ajustando modulos iniciais para funcionar o jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AutenticacaoModule } from './autenticacao/autenticacao.module';
 import { EmpresaModule } from './empresa/empresa.module';
+import { FolhaPagamentoModule } from './folha-pagamento/folha-pagamento.module';
 import { FuncionarioModule } from './funcionario/funcionario.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsuarioModule } from './usuario/usuario.module';
-import { FolhaPagamentoModule } from './folha-pagamento/folha-pagamento.module';
-import { AutenticacaoModule } from './autenticacao/autenticacao.module';
 
 @Module({
-  imports: [PrismaModule, EmpresaModule, FuncionarioModule, UsuarioModule, FolhaPagamentoModule, AutenticacaoModule],
+  imports: [PrismaModule, EmpresaModule,
+    FuncionarioModule, UsuarioModule,
+    FolhaPagamentoModule, AutenticacaoModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { useContainer } from 'class-validator';
 import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 
@@ -8,16 +6,6 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
-
-  useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
   await app.listen(process.env.PORT ?? 3000);
 }
